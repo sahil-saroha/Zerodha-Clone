f0cf91d58a630dad70dd134ff64054f0821c8f5f
Sahil Saroha <sarohasahil4406@gmail.com>
2024-11-26 00:06:13 +0530

    Initial commit with React and Node projects

---
+++ backend/model/HoldingsModel.js
@@ -0,0 +1,8 @@
+const {model} = require("mongoose");
+
+
+const {HoldingSchema} = require("../schemas/HoldingSchema");
+
+const HoldingsModel = new model("holding",HoldingSchema);
+
+module.exports = {HoldingsModel};
